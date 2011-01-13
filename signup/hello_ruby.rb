a94017acd7d7ecc8184bfa83c960c031cc2413eb
Christie Koehler <christie@shopigniter.com>
2011-01-13 14:41:44 -0800

    Completed the signup task

--- signup/hello_ruby.rb
+++ signup/hello_ruby.rb
@@ -1,7 +1,7 @@
 # Put your name (or username/alias) between the quotes
-name = ""
+name = "Christie Koehler"
 
 # Put your favorite fruit between the quotes
-favorite_fruit = ""
+favorite_fruit = "stravberries"
 
 puts "Hi!  My name is #{name} and my favorite fruit is #{favorite_fruit}"
