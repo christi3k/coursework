76d550ba9e098d69a1f7c4abee4ad306906edc1e
Andy Lindeman <alindeman@gmail.com>
2011-01-13 15:30:45 -0600

    Add a really bizarre script for the sign up task

---
+++ signup/hello_ruby.rb
@@ -0,0 +1,7 @@
+# Put your name (or username/alias) between the quotes
+name = ""
+
+# Put your favorite fruit between the quotes
+favorite_fruit = ""
+
+puts "Hi!  My name is #{name} and my favorite fruit is #{favorite_fruit}"
